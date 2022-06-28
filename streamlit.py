ba2809462733771dd010ff6be180c338b7ce1412
Hiroka-Tamura-techlabs <89800530+Hiroka-Tamura-techlabs@users.noreply.github.com>
2022-06-28 13:37:23 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -30,10 +30,31 @@ fig = go.Figure(data=
 st.plotly_chart(fig)
 
 import plotly.express as px
-df = px.data.iris()
-fig2 = px.parallel_coordinates(df, color="species_id",
+iris = px.data.iris()
+fig2 = px.parallel_coordinates(iris, color="species_id",
                               dimensions=['sepal_width', 'sepal_length', 'petal_width',
                                           'petal_length'],
                               color_continuous_scale=px.colors.diverging.Tealrose,
                               color_continuous_midpoint=2)
 st.plotly_chart(fig2)
+
+df = pd.read_csv("https://raw.githubusercontent.com/bcdunbar/datasets/master/iris.csv")
+
+fig3 = go.Figure(data=
+    go.Parcoords(
+        line = dict(color = df['species_id'],
+                   colorscale = [[0,'purple'],[0.5,'lightseagreen'],[1,'gold']]),
+        dimensions = list([
+            dict(range = [0,8],
+                constraintrange = [4,8],
+                label = 'Sepal Length', values = df['sepal_length']),
+            dict(range = [0,8],
+                label = 'Sepal Width', values = df['sepal_width']),
+            dict(range = [0,8],
+                label = 'Petal Length', values = df['petal_length']),
+            dict(range = [0,8],
+                label = 'Petal Width', values = df['petal_width'])
+        ])
+    )
+)
+st.plotly_chart(fig3)
