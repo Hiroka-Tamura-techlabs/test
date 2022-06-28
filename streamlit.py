6ef1f200e50d683ca810c6e23f651ba25df55578
Hiroka-Tamura-techlabs <89800530+Hiroka-Tamura-techlabs@users.noreply.github.com>
2022-06-28 14:41:02 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -40,8 +40,7 @@ store2 = st.sidebar.selectbox("Select store 2:",slist)
 fig5= px.parallel_categories(a[(a['the_store_id']==store1) | (a['the_store_id']==store2)], 
                              dimensions=['meal_time', 'release_branch', 'escalated_ind'],
                              labels={'meal_time':'Meal Time', 'release_branch':'Release Branch', 'escalated_ind':'Escalated or Not'},
-                             color='the_store_id', color_discrete_map={store1: 'lightcyan', 
-                                                   store2: 'royalblue'})
+                             color='the_store_id')
 
 st.plotly_chart(fig5)
 
