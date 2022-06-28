d819bb0e42a5bc0b5ecbb842debaedbec0b6f80c
Hiroka-Tamura-techlabs <89800530+Hiroka-Tamura-techlabs@users.noreply.github.com>
2022-06-28 14:01:53 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,7 +38,7 @@ slist = a['the_store_id'].unique()
 store1 = st.sidebar.selectbox("Select store 1:",slist)
 store2 = st.sidebar.selectbox("Select store 2:",slist)
 
-fig5= px.parallel_categories(a[(a['the_store_id']==store1) | a[(a['the_store_id']==store2)], dimensions=['meal_time', 'release_branch', 'escalated_ind'],
+fig5= px.parallel_categories(a[(a['the_store_id']==store1) | (a['the_store_id']==store2)], dimensions=['meal_time', 'release_branch', 'escalated_ind'],
                               color='the_store_id',
                                labels={'meal_time':'Meal Time', 'release_branch':'Release Branch', 'escalated_ind':'Escalated or Not'})
 
